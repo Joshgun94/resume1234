0696e10fac031d8928b8bfc8cc5b42b6226b5b5e
Joshgun94 <gulalizada.joshgun@gmail.com>
2021-12-24 02:43:31 +0400

    deyishiklik var

---
+++ Demo1134/src/demo1134/Demo1134.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo1134;
+
+/**
+ *
+ * @author gulal
+ */
+public class Demo1134 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
